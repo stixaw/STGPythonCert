8333c817ec207b3571b7fc0d3ee4a8bc73fad980
stixaw <stixaw@gmail.com>
2019-08-15 19:10:46 -0600

    Challenge 4 working

--- challenges/challenge4.py
+++ challenges/challenge4.py
@@ -1,7 +1,8 @@
-import os
-import sys
-import numtostring
 import fibonacci
+import numtostring
+import inflect
+
+p = inflect.engine()
 
 
 def fib_looper(n):
@@ -17,9 +18,29 @@ def fib_looper(n):
             fib_list.append(fib_list[i - 2] + fib_list[i - 1])
             i += 1
         for num in fib_list:
-            num_word = numtostring.get_num(num)
-            print("{} ... {}".format(num, num_word), end=' ')
+            num_word = p.number_to_words(num)
+            print("{} ... {}".format(num, num_word))
 
+
+def fib_looper2(n):
+    # 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610
+    if n <= 1:
+        result = n
+        print(n)
+
+    else:
+        fib_list = [0, 1]
+        i = 2
+        while i < n + 1:
+            fib_list.append(fib_list[i - 2] + fib_list[i - 1])
+            i += 1
+        for num in fib_list:
+            num_word = numtostring.is_number_positive(num)
+            print("{} ... {}".format(num, num_word))
+
+
+# , end=' '
 
 if __name__ == '__main__':
-    fib_looper(8)
+    fib_looper(30)
+    fib_looper2(5)
--- challenges/numtostring.py
+++ challenges/numtostring.py
@@ -1,39 +1,60 @@
 # Number to String module
 
+ones = {
+    0: '', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six',
+    7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve',
+    13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen',
+    17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+tens = {
+    2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty', 6: 'sixty',
+    7: 'seventy', 8: 'eighty', 9: 'ninety'}
+illions = {
+    1: 'thousand', 2: 'million', 3: 'billion', 4: 'trillion', 5: 'quadrillion',
+    6: 'quintillion', 7: 'sextillion', 8: 'septillion', 9: 'octillion',
+    10: 'nonillion', 11: 'decillion'}
 
-def get_num(number):
-    three_digit_num = ""
-    s_number = "{0}".format(number)
 
-    digits = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
-              'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+def is_number_positive(i):
+    """
+    Convert an integer in to it's word representation.
 
-    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    say_number(i: integer) -> string
+    """
+    if i < 0:
+        return _join('negative', _num_to_word(-i))
+    if i == 0:
+        return 'zero'
+    return _num_to_word(i)
 
-    if len(s_number) < 3 and number < 20:
-        three_digit_num = digits[number]
 
-    else:
+def _num_to_word(i):
+    if i < 20:
+        return ones[i]
+    if i < 100:
+        return _join(tens[i // 10], ones[i % 10])
+    if i < 1000:
+        return _divide(i, 100, 'hundred')
+    for illions_number, illions_name in illions.items():
+        if i < 1000 ** (illions_number + 1):
+            break
+    return _divide(i, 1000 ** illions_number, illions_name)
 
-        # get hundred value
-        if len(s_number) == 3:
-            num: int = s_number[len(s_number)-3: 1]
-            if num > 0:
-                three_digit_num = three_digit_num + digits[num] + " hundred"
-            else:
-                three_digit_num = three_digit_num
 
-        # get tens value
-        tens_num: int = s_number[len(s_number)-2: 3]
-        if tens_num == 00:
-            three_digit_num = three_digit_num
+def _divide(dividend, divisor, magnitude):
+    return _join(
+        _num_to_word(dividend // divisor),
+        magnitude,
+        _num_to_word(dividend % divisor),
+    )
 
-        if tens_num > 0:
-            if tens_num >= 20:
-                three_digit_num = three_digit_num + " " + tens[tens_num[0]]
 
-            if tens_num > 1 and tens_num <= 19:
-                three_digit_num = three_digit_num + " " + digits[tens_num]
-            else:
-                one_num = tens_num[1]
-                three_digit_num = three_digit_num + " " + digits[one_num]
+def _join(*args):
+    return ' '.join(filter(bool, args))
+
+
+def test_say_number(data, expected_output):
+    """Test cases for say_number(i)."""
+    output = is_number_positive(data)
+    assert output == expected_output, \
+        "\n    for:      {}\n    expected: {}\n    got:      {}".format(
+            data, expected_output, output)
